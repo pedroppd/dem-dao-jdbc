478a059582c89985ea844a9ac225eed4490d0844
Pedro Dantas <pedro_pdantas@hotmail.com>
2019-07-07 09:30:54 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,9 +10,9 @@ public class Program {
 	public static void main(String[] args) {
 		Department dp = new Department(1, "Books");
 		System.out.println(dp);
-		
+
 		Seller seller = new Seller(12, "Bob", "bob@gmail.com", new Date(), 1200.0, dp);
-	System.out.println(seller);
+		System.out.println(seller);
 	}
 
 }
---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,41 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import domain.Department;
+import domain.Seller;
+import model.dao.SellerDAO;
+
+public class SellerDaoJDBC implements SellerDAO{
+
+	@Override
+	public void insert(Seller seller) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Seller seller) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Department> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Department findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
